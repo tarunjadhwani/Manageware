cd838207353922aa6a6c381730dd5928e3fec1df
nEo <neo@neo-Inspiron-1420.(none)>
2014-01-31 21:25:29 +0530

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Manageware::Application.routes.draw do
-  get "users/new"
+  resources :users
+  match '/signup',  to: 'users#new',            via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
