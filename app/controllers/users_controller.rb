383fe43acb7600ea5068743541b3a8fc272f1997
nEo <neo@neo-Inspiron-1420.(none)>
2014-01-31 23:01:40 +0530

    User signin & signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,17 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
-  	if(@user.save)
+  	if @user.save
   		sign_in @user
+      redirect_to show_path
   		flash[:success] = "User created successfully"
   	else
   		render 'new'
   	end
   	
   end
+
+  private
 
   def user_params
 	params.require(:user).permit(:name, :email, :password, :password_confirmation)	
